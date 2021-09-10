ce45a0b6cd3ec4726e4881763cb964a92332f93d
Miguel <miferco97@gmail.com>
2021-09-10 16:35:05 +0200

    improved results_net_vs_aruco.py

--- results_net_vs_aruco.py
+++ results_net_vs_aruco.py
@@ -12,12 +12,14 @@ import numpy as np
 cameraMatrix = np.array([[790.8989920514197, 0.0, 670.332791421756],[0.0, 789.6808338497912, 370.6481124492188], [0.0, 0.0, 1.0]])
 distCoeffs   = np.array([-0.03448682771417174, -0.055932650937412745, 0.11969799783448262, -0.09163586323944228])
 
+
 # Load image
 IMAGES_PATH = './Dataset/Data_Adam/gates/'
 ARUCO_TXT_FILE = './aruco_pose.txt'
 PNP_TXT_FILE = './estimation_net.txt'
 
-SHOW_AXIS = True
+SHOW_AXIS = False
+# SHOW_AXIS = True
 
 aruco_lines = []
 pnp_lines = []
@@ -55,10 +57,17 @@ def get_pnp_pose(image_name):
             return tvec, rvec
 
 
+
 def computeMetrics(aruco_t_vec,aruco_r_vec, pnp_t_vec,pnp_r_vec):
     
-    dist_est = np.linalg.norm(aruco_t_vec - pnp_t_vec)
+    dist_error = np.linalg.norm(aruco_t_vec - pnp_t_vec)
+    est_dist = np.linalg.norm(aruco_t_vec)
     
+    return [dist_error,est_dist]
+
+
+
+metric_list = None
 for image_name in extract_common_images():
     aruco_t_vec, aruco_r_vec = get_aruco_pose(image_name)
     pnp_t_vec, pnp_r_vec     = get_pnp_pose(image_name)
@@ -73,14 +82,58 @@ for image_name in extract_common_images():
         # cv2.imshow('aruco_axis', aruco_axis)
         cv2.imshow('pnp_axis', pnp_axis)
         cv2.waitKey(0) 
-    dist_est = computeMetrics(aruco_t_vec,aruco_r_vec, pnp_t_vec,pnp_r_vec)
-    print(image_name, dist_est)
+        
+    metrics = computeMetrics(aruco_t_vec,aruco_r_vec, pnp_t_vec,pnp_r_vec)
     
-    # except:
-    #     pass    
+    if metric_list is None:
+        metric_list = np.array([metrics])
+    else:
+        metric_list = np.concatenate((metric_list, np.array([metrics])), axis=0)
+        
+    
+    
 
-cv2.destroyAllWindows()
-    
-    
-    
+if SHOW_AXIS:
+    cv2.destroyAllWindows()
 
+
+STEP = 1
+
+def compute_total_metrics():
+    mean_error =np.mean(metric_list[:,0])
+    var_error =np.var(metric_list[:,0])
+    max_dist = int(np.max(metric_list[:,1]))
+    max_dist += max_dist % STEP 
+        
+    metrics_per_range = []
+    
+    for i in range(max_dist//STEP):
+        temp_metric = []
+        for index,elem in enumerate(metric_list[:,1]):
+            if elem >= i*STEP and elem <= (i+1)*STEP:
+                temp_metric.append(metric_list[index,0])
+        
+        metrics_per_range.append(temp_metric)
+            
+    print('mean_error:',mean_error)
+    print('var_error:',var_error)
+    print('max_dist:',max_dist)
+    print('metrics_per_range',len(metrics_per_range))
+    return [mean_error,var_error,max_dist,metrics_per_range]
+
+mean_error,var_error,max_dist,metrics_per_range = compute_total_metrics()
+
+# DRAW PLOT
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('Basic Plot')
+ax1.boxplot(metrics_per_range)
+ax1.set_yticks(np.arange(0, max_dist, STEP))
+plt.show()
+
+
+
+    
